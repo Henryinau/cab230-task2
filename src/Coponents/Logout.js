59490f40856e72389159f9d541c4e3a73d0feed1
Henry <1153426669@qq.com>
2023-05-09 22:43:06 +1000

    logout changes

--- src/Coponents/Logout.js
+++ src/Coponents/Logout.js
@@ -1,38 +1,9 @@
 import Nav from 'react-bootstrap/Nav';
 
 const logout = (onSuccess, onError) => {
-  const API_URL = 'http://sefdb02.qut.edu.au:3000';
-  const url = `${API_URL}/user/logout`;
-
-  const refreshToken = localStorage.getItem('refreshToken');
-  if (!refreshToken) {
-    onError('No refresh token found');
-    return;
-  }
-
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${refreshToken}`
-    },
-  })
-  .then((res) => {
-    if (res.status === 200) {
-      localStorage.removeItem('refreshToken');
-      localStorage.removeItem('token');
-      onSuccess();
-    } else if (res.status === 400) {
-      throw new Error('Invalid refresh request');
-    } else if (res.status === 401) {
-      throw new Error('Unauthorized');
-    } else if (res.status === 429) {
-      throw new Error('Rate limit exceeded');
-    } else {
-      throw new Error('Unknown error');
-    }
-  })
-  .catch((error) => onError(error.message));
+  localStorage.removeItem('token');
+  localStorage.removeItem('refreshToken');
+  onSuccess();
 };
 
 export default function LogoutBt()
