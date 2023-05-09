648156d4961b3a435529433fc7013a9d280d20f3
Henry <1153426669@qq.com>
2023-05-09 16:42:48 +1000

    personalPage

--- src/Pages/PersonalPage/PersonalPage.js
+++ src/Pages/PersonalPage/PersonalPage.js
@@ -99,13 +99,13 @@
 
 
 
-import React, { useState, useEffect } from "react";
+/*import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function PersonDetails() {
   const [person, setPerson] = useState({});
   const [currentPage, setCurrentPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const bearerToken = localStorage.getItem("token");
 
   const { id } = useParams();
@@ -136,6 +136,8 @@ export default function PersonDetails() {
   const pageCount = Math.ceil(person.roles?.length / rowsPerPage);
   const pageData = person.roles?.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
 
+  //-----------------------
+  
   return (
     <div>
       <h2>{person.name}</h2>
@@ -176,12 +178,137 @@ export default function PersonDetails() {
           <button onClick={handleNextClick} disabled={currentPage === pageCount - 1}>
                  Next
            </button>
-    </div>
+          </div>
 </>
             )}
    </div>
    );
   }
 
+
+*/
+
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './PersonalPage.css'
+
+export default function PersonDetails() {
+  const [person, setPerson] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [scores, setScores] = useState([]);
+  const bearerToken = localStorage.getItem("token");
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPersonData = async () => {
+      const response = await fetch(`http://sefdb02.qut.edu.au:3000/people/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + bearerToken,
+        },
+      });
+      const data = await response.json();
+      setPerson(data);
+    };
+    fetchPersonData();
+  }, [id]);
+
+  const handlePrevClick = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const pageCount = Math.ceil(person.roles?.length / rowsPerPage);
+  const pageData = person.roles?.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+  useEffect(() => {
+    if (person.roles) {
+      // Count the number of scores in different ranges
+      const counts = Array(10).fill(0);
+      for (const role of person.roles) {
+        const score = Math.floor(role.imdbRating);
+        if (score >= 0 && score <= 9) {
+          counts[score]++;
+        }
+      }
+      // Convert counts to the format expected by Recharts
+      const data = counts.map((count, index) => {
+        return {
+          scoreRange: `${index}-${index+1}`,
+          count: count,
+        };
+      });
+      setScores(data);
+    }
+  }, [person]);
+
+  //-----------------------
+  
+  return (
+    <div className="personContainer">
+      <h1 className="name-title">{person.name}</h1>
+      <p className="life">
+        BirthYear: {person.birthYear} - DeathYear: {person.deathYear}
+      </p>
+      {person.roles && (
+        <div className="tableContainer">
+          <table className="table-personal">
+            <thead>
+              <tr>
+                <th>MovieName</th>
+                <th>MovieId</th>
+                <th>Category</th>
+                <th>Characters</th>
+                <th>ImdbRating</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pageData?.map((role) => (
+                <tr key={role.movieId}>
+                  <td>{role.movieName}</td>
+                  <td>{role.movieId}</td>
+                  <td>{role.category}</td>
+                  <td>{role.characters.join(",") || "N/A"}</td>
+                  <td>{role.imdbRating}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          
+     </div>
+            )}
+            <div className="button-personal">
+            <button onClick={handlePrevClick} disabled={currentPage === 0}>
+              Previous
+            </button>
+            <span>
+              Page {currentPage + 1} of {pageCount}
+            </span>
+          <button onClick={handleNextClick} disabled={currentPage === pageCount - 1}>
+                 Next
+           </button>
+          </div>
+            <div className='chart'>
+      <BarChart width={800} height={400} data={scores}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="scoreRange" />
+            <YAxis  domain={[0,25]} tickCount={6}/>
+         
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+          </div>
+    </div>
+  );
+}
 
 
--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -1,93 +1,99 @@
 
- /*
-import React, { useState, useEffect } from "react";
+ 
+//import React, { useState, useEffect } from "react";
+//
+//function MovieSearch() {
+//const [movies, setMovies] = useState([]);
+//const [searchTerm, setSearchTerm] = useState("");
+//const [selectedYear, setSelectedYear] = useState("");
+//const [years, setYears] = useState([]);
+//
+//const [currentPage, setCurrentPage] = useState(0);
+//const [rowsPerPage, setRowsPerPage] = useState(5);
+//
+//useEffect(() => {
+//const fetchData = async () => {
+//try {
+//const response = await fetch("http://sefdb02.qut.edu.au:3000/movies/search");
+//if (!response.ok) {
+//throw new Error("Network response was not ok");
+//}
+//const data = await response.json();
+//setMovies(data.data);
+//const years = Array.from({ length: 34 }, (_, index) => 1990 + index);
+//setYears(years);
+//} catch (error) {
+//console.error("There was a problem fetching the data: ", error);
+//}
+//};
+//fetchData();
+//}, []);
+//
+//if (movies.length === 0 || years.length === 0) {
+//return <div>Loading...</div>;
+//}
+//
+//const filteredMovies = movies.filter((movie) => {
+//return (
+//movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+//(selectedYear === "" || parseInt(movie.year) === parseInt(selectedYear))
+//);
+//});
+//
+//
+//return (
+//<div>
+//<h2>Movie List</h2>
+//<div>
+//<label>
+//Search movies:
+//<input
+//type="text"
+//placeholder="Enter title..."
+//value={searchTerm}
+//onChange={(e) => setSearchTerm(e.target.value)}
+///>
+//</label>
+//<label>
+//Filter by year:
+//<select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+//<option value="">All</option>
+//{years.map((year) => (
+//<option key={year} value={year}>
+//{year}
+//</option>
+//))}
+//</select>
+//</label>
+//</div>
+//<table>
+//  <thead>
+//    <tr>
+//    <th>Title</th>
+//    <th>Year</th>
+//    <th>IMDB Rating</th>
+//    <th>Rotten Tomatoes Rating</th>
+//    <th>Metacritic Rating</th>
+//    <th>Classification</th>
+//    </tr>
+//  </thead>
+//    <tbody>
+//    {filteredMovies.map((movie) => (
+//    <tr key={movie.imdbID}>
+//      <td>{movie.title}</td>
+//      <td>{movie.year}</td>
+//      <td>{movie.imdbRating}</td>
+//      <td>{movie.rottenTomatoesRating}</td>
+//      <td>{movie.metacriticRating}</td>
+//      <td>{movie.classification}</td>
+//    </tr>
+//    ))}
+//    </tbody>
+//</table> </div> ); }
+//export default MovieSearch;
 
-function MovieSearch() {
-const [movies, setMovies] = useState([]);
-const [searchTerm, setSearchTerm] = useState("");
-const [selectedYear, setSelectedYear] = useState("");
-const [years, setYears] = useState([]);
 
-useEffect(() => {
-const fetchData = async () => {
-try {
-const response = await fetch("http://sefdb02.qut.edu.au:3000/movies/search");
-if (!response.ok) {
-throw new Error("Network response was not ok");
-}
-const data = await response.json();
-setMovies(data.data);
-const years = Array.from({ length: 34 }, (_, index) => 1990 + index);
-setYears(years);
-} catch (error) {
-console.error("There was a problem fetching the data: ", error);
-}
-};
-fetchData();
-}, []);
 
-if (movies.length === 0 || years.length === 0) {
-return <div>Loading...</div>;
-}
-
-const filteredMovies = movies.filter((movie) => {
-return (
-movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
-(selectedYear === "" || parseInt(movie.year) === parseInt(selectedYear))
-);
-});
-
-return (
-<div>
-<h2>Movie List</h2>
-<div>
-<label>
-Search movies:
-<input
-type="text"
-placeholder="Enter title..."
-value={searchTerm}
-onChange={(e) => setSearchTerm(e.target.value)}
-/>
-</label>
-<label>
-Filter by year:
-<select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
-<option value="">All</option>
-{years.map((year) => (
-<option key={year} value={year}>
-{year}
-</option>
-))}
-</select>
-</label>
-</div>
-<table>
-<thead>
-<tr>
-<th>Title</th>
-<th>Year</th>
-<th>IMDB Rating</th>
-<th>Rotten Tomatoes Rating</th>
-<th>Metacritic Rating</th>
-<th>Classification</th>
-</tr>
-</thead>
-<tbody>
-{filteredMovies.map((movie) => (
-<tr key={movie.imdbID}>
-  <td>{movie.title}</td>
-  <td>{movie.year}</td>
-  <td>{movie.imdbRating}</td>
-  <td>{movie.rottenTomatoesRating}</td>
-  <td>{movie.metacriticRating}</td>
-  <td>{movie.classification}</td>
-</tr>
-))}
-</tbody>
-</table> </div> ); }
-export default MovieSearch;
-*/
 
 import './SearchPage.css'
 import React, { useState, useEffect, useRef } from "react";
@@ -207,3 +213,5 @@ export default function MovieSearch() {
     </div>
     );
 } 
+
+
