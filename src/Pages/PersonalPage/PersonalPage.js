ac353173a29a112d39dd9e979c630ce99bffe010
Henry <1153426669@qq.com>
2023-05-10 20:09:45 +1000

    changes

--- src/Pages/PersonalPage/PersonalPage.js
+++ src/Pages/PersonalPage/PersonalPage.js
@@ -188,7 +188,7 @@ export default function PersonDetails() {
 */
 
 
-
+/*
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
@@ -316,4 +316,152 @@ export default function PersonDetails() {
   );
 }
 
+*/
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import './PersonalPage.css'
+
+export default function PersonDetails() {
+  const [person, setPerson] = useState({});
+  const [currentPage, setCurrentPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [scores, setScores] = useState([]);
+  const bearerToken = localStorage.getItem("token");
+
+  const { id } = useParams();
+
+  const [gridApi, setGridApi] = useState(null);
+  
+
+  useEffect(() => {
+    const fetchPersonData = async () => {
+      const response = await fetch(`http://sefdb02.qut.edu.au:3000/people/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + bearerToken,
+        },
+      });
+      const data = await response.json();
+      setPerson(data);
+    };
+    fetchPersonData();
+  }, [id]);
+
+  const handlePrevClick = () => {
+    if (currentPage > 0) {
+      gridApi.paginationGoToPreviousPage();
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+  
+  const handleNextClick = () => {
+    if (currentPage < pageCount - 1) {
+      gridApi.paginationGoToNextPage();
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const pageCount = Math.ceil(person.roles?.length / rowsPerPage);
+  const pageData = person.roles?.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
+
+
+
+  useEffect(() => {
+    if (person.roles) {
+      // Count the number of scores in different ranges
+      const counts = Array(10).fill(0);
+      for (const role of person.roles) {
+        const score = Math.floor(role.imdbRating);
+        if (score >= 0 && score <= 9) {
+          counts[score]++;
+        }
+      }
+      // Convert counts to the format expected by Recharts
+      const data = counts.map((count, index) => {
+        return {
+          scoreRange: `${index}-${index+1}`,
+          count: count,
+        };
+      });
+      setScores(data);
+    }
+  }, [person]);
+
+  const tableColumns = [
+    { headerName: "Movie Name", field: "movieName" },
+    { headerName: "Movie Id", field: "movieId" },
+    { headerName: "Category", field: "category" },
+    { headerName: "Characters", field: "characters", valueGetter: (params) => params.data.characters.join(",") || "N/A" },
+    { headerName: "IMDb Rating", field: "imdbRating" }
+  ];
+
+  
+  //-----------------------
+  
+  return (
+    <div className="personContainer">
+      <h1 className="name-title">{person.name}</h1>
+      <p className="life">
+        BirthYear: {person.birthYear} - DeathYear: {person.deathYear}
+      </p>
+      {person.roles && (
+        <div className="tableContainer">
+          <div className="ag-theme-alpine" style={{ height: "400px", width: "1100px" }}>
+            <AgGridReact
+
+             onGridReady={(params) => {
+              setGridApi(params.api);
+                 }}
+          
+              rowData={pageData}
+              columnDefs={tableColumns}
+              pagination={true}
+              paginationPageSize={10} // 每页显示的行数
+              
+              
+              
+            />
+          </div>
+
+
+          <div className="button-personal">
+            <button onClick={handlePrevClick} disabled={currentPage === 0}>
+              Previous
+            </button>
+            <span>
+              Page {currentPage + 1} of {pageCount}
+            </span>
+          <button onClick={handleNextClick} disabled={currentPage === pageCount - 1}>
+                 Next
+           </button>
+          </div>
+          
+          <div className='chart'>
+          <BarChart width={800} height={400} data={scores}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="scoreRange" />
+            <YAxis  domain={[0,25]} tickCount={6}/>
+         
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#8884d8" />
+          </BarChart>
+          </div>
+       </div>
+      
+            )}
+            
+       
+          
+    </div>
+  );
+}
 
--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -217,3 +217,137 @@ export default function MovieSearch() {
 } 
 
 
+
+/*
+import './SearchPage.css'
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+
+export default function MovieSearch() {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedYear, setSelectedYear] = useState("");
+    const [years, setYears] = useState([]);
+    const [loadedMovies, setLoadedMovies] = useState(20); // 初始显示20条电影数据
+    const [isLoading, setIsLoading] = useState(false);   // 标记是否正在加载数据中
+    const [allMoviesLoaded, setAllMoviesLoaded] = useState(false); // 标记是否已经全部加载完成
+    const containerRef = useRef(null); // 用来获取滚动容器的引用
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`http://sefdb02.qut.edu.au:3000/movies/search?limit=${loadedMovies}`);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          setMovies(data.data);
+          const years = Array.from({ length: 34 }, (_, index) => 1990 + index);
+          setYears(years);
+          setIsLoading(false);
+          if (data.data.length < 20) {
+            setAllMoviesLoaded(true);
+          } else if (movies.length === loadedMovies) {
+            setTimeout(() => {
+              setAllMoviesLoaded(true);
+            }, 1000)
+          }
+        } catch (error) {
+          console.error("There was a problem fetching the data: ", error);
+        }
+      };
+      fetchData();
+    }, [loadedMovies, movies.length]);
+  
+    useEffect(() => {
+      const container = containerRef.current;
+      const handleScroll = () => {
+        const { scrollTop, offsetHeight, scrollHeight } = container;
+        if (scrollTop + offsetHeight >= scrollHeight - offsetHeight) {
+          if (!isLoading && !allMoviesLoaded) {
+            setLoadedMovies((prevLoadedMovies) => prevLoadedMovies + 20);
+          }
+        }
+      };
+      container.addEventListener("scroll", handleScroll);
+      return () => {
+        container.removeEventListener("scroll", handleScroll);
+      };
+    }, [isLoading, allMoviesLoaded]);
+  
+    const filteredMovies = movies.filter((movie) => {
+      return (
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (selectedYear === "" || parseInt(movie.year) === parseInt(selectedYear))
+      );
+    });
+  
+    const allMoviesLoadedMsg = allMoviesLoaded ? "All movies loaded." : "Loading more movies...";
+
+
+    function onGridReady(params) {
+      const gridApi = params.api;
+      gridApi.sizeColumnsToFit();
+    }
+    const columnDefs = [
+      { field: 'title', headerName: 'Title', sortable: true, filter: true },
+      { field: 'year', headerName: 'Year', sortable: true, filter: true },
+      { field: 'imdbRating', headerName: 'IMDB Rating', sortable: true, filter: true },
+      { field: 'rottenTomatoesRating', headerName: 'Rotten Tomatoes Rating', sortable: true, filter: true },
+      { field: 'metacriticRating', headerName: 'Metacritic Rating', sortable: true, filter: true },
+      { field: 'classification', headerName: 'Classification', sortable: true, filter: true }
+    ];
+    
+    const rowData = filteredMovies.map((movie) => ({
+      title: <Link to={`/movies/details/${movie.imdbID}`}>{movie.title}</Link>,
+      year: movie.year,
+      imdbRating: movie.imdbRating,
+      rottenTomatoesRating: movie.rottenTomatoesRating,
+      metacriticRating: movie.metacriticRating,
+      classification: movie.classification}));
+
+    return (
+  <div ref={containerRef}>
+    <h1 className='search-title'>Movie List</h1>
+    <div>
+      <label>
+        Search movies:
+        <input
+          type="text"
+          placeholder="Enter title..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </label>
+      <label>
+        Filter by year:
+        <select value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+          <option value="">All</option>
+          {years.map((year) => (
+            <option key={year} value={year}>{year}</option>
+          ))}
+        </select>
+      </label>
+    </div>
+
+    <div className="ag-theme-alpine" style={{ height: '500px', width: '100%' }}>
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={rowData}
+        pagination={true}
+        paginationPageSize={20}
+        onGridReady={onGridReady}
+      />
+    </div>
+
+    {allMoviesLoadedMsg}
+  </div>
+);
+
+} 
+*/
